d190f1354531f2129854c419eee8aeff99e8df69
Leung Yu <1065617282@qq.com>
2022-07-09 18:17:19 +0800

    commit add desc

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -33,13 +33,15 @@ describe('Chart.vue', () => {
           const numberText = chartBar.find('.chart-content-element-show-value').text()
           const svg = chartBar.find('svgicon-stub')
           if (numberText === '') {
-            expect(chartBar.attributes('style')).toBe('height: 87px;')
+            // 没有值的情况使用灰色css类
             expect(chartBar.findAll('.chart-content-element-icontainer-incontainer-show-none')).toHaveLength(1)
             expect(svg.attributes('iconclass')).toBe('wenhao')
           } else if (Number(numberText) >= 90) {
+            // 值大于等于90的情况使用橙色css类
             expect(chartBar.findAll('.chart-content-element-icontainer-incontainer-show-well')).toHaveLength(1)
             expect(svg.attributes('iconclass')).toBe('xixiao')
           } else {
+            // 值小于90的情况使用绿色css类
             expect(chartBar.findAll('.chart-content-element-icontainer-incontainer-show-normal')).toHaveLength(1)
             expect(svg.attributes('iconclass')).toBe('weixiao')
           }
@@ -52,9 +54,11 @@ describe('Chart.vue', () => {
     const chartKeys = wrapper.findAll('.chart-key-element')
     const chartKeyNormal = chartKeys.at(1)
     const chartKeyWell = chartKeys.at(3)
+    // 点击第二个条形图的坐标
     chartKeyNormal.trigger('click')
     await new Promise((resolve) => {
       setTimeout(() => {
+        // 使用被选中的css类
         const chartBars = wrapper.findAll('.chart-bar-background')
         const chartBarNormal = chartBars.at(1)
         expect(chartBarNormal.attributes('class').includes('chart-content-element-initial-normal-selected')).toBeTruthy()
@@ -65,7 +69,10 @@ describe('Chart.vue', () => {
     await new Promise((resolve) => {
       setTimeout(() => {
         const chartBars = wrapper.findAll('.chart-bar-background')
+        const chartBarNormal = chartBars.at(1)
         const chartBarWell = chartBars.at(3)
+        // 使用被选中的css类,同时上一个被选中的坐标对应的条形图解除选中状态
+        expect(!(chartBarNormal.attributes('class').includes('chart-content-element-initial-normal-selected'))).toBeTruthy()
         expect(chartBarWell.attributes('class').includes('chart-content-element-initial-well-selected')).toBeTruthy()
         resolve()
       }, 1000)
